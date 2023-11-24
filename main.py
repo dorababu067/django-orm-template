58cf92a1da56278d9b5c6e25e44d010fb1835df3
dorababu067 <dorababua67@gmail.com>
2023-11-24 06:23:03 +0530

    code cleanup for template

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
-############################################################################
-## Django ORM Standalone Python Template
-############################################################################
-""" Here we'll import the parts of Django we need. It's recommended to leave
-these settings as is, and skip to START OF APPLICATION section below """
+# django setup
+import setup
 
-# Turn off bytecode generation
-import sys
+from db.models import User
 
-sys.dont_write_bytecode = True
-
-# Django specific settings
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-import django
-
-django.setup()
-
-# Import your models for use in your script
-from db.models.user import *
-
-############################################################################
-## START OF APPLICATION
-############################################################################
-""" Replace the code below with your own """
 
 # Seed a few users in the database
 User.objects.create(name="Dan")
---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+# # Set the path to your custom settings module
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# sys.path.append(BASE_DIR)
+
+
+# Set the DJANGO_SETTINGS_MODULE environment variable to the custom path
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+# Import Django after setting the environment variable
+import django
+
+django.setup()
